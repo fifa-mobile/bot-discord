9bc268bf3f7f9d432749ad932ef89287ff231288
yuulye <yuulye@gmail.com>
2020-10-06 04:14:09 +0700

    add only emoji deletion message feature

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
-const tokenConfig = fs.readFileSync('configs/token.json');
-let tokenData = false;
+
+let configFilePath = 'configs/token.json';
+let config = fs.readFileSync(configFilePath);
+let data = false;
 try {
-  tokenData = JSON.parse(tokenConfig);
+  data = JSON.parse(config);
 } catch(e) {
-  throw new Error('Error parsing configs/token.json');
+  throw new Error(`Error parsing ${configFilePath}`);
 }
-const token = tokenData.value;
+const token = data.value;
+
+configFilePath = 'configs/logs-channel-id.json';
+config = fs.readFileSync(configFilePath);
+data = false;
+try {
+  data = JSON.parse(config);
+} catch(e) {
+  throw new Error(`Error parsing ${configFilePath}`);
+}
+const logsChannelID = data.value;
 
 const Discord = require('discordjs-stable');
 const client = new Discord.Client();
@@ -16,7 +28,49 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  console.log(message.content);
+  /* #22 emoji only message deletion feature */
+  console.log(
+    'reading a messsage:'
+    , message.content
+    , `from channel #`, message.channel.id
+  );
+  let oldStr = str = message.content;
+  const re =
+    /(:[^:\s]+:|<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>)/g;
+    str = str.replace(re, '');
+
+  if (oldStr !== str) {
+    console.log('server emojis removed', str);
+  }
+
+  str = str.replace(
+    /([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, ''
+  );
+
+  str = str.replace(/\s/g, "");
+
+  let newStr = str.replace(
+    String.fromCharCode(65039), ''
+  );
+
+  let count = 0;
+  while (str !== newStr) {
+    str = newStr;
+    newStr = str.replace(String.fromCharCode(65039), '');
+    if (count > 10) break;
+  }
+
+  if (str.length === 0) {
+    console.log('empty content detected!');
+    client.channels.get(logsChannelID).send(
+      `a message from <@${message.author.id}> on `
+      + `<#${message.channel.id}> deleted:` + '\n'
+      + message.content
+    );
+    message.delete();
+  }
+  /* #22 emoji only message deletion feature */
+
   if (message.content === '&ping') {
     const pings = [
       "Testing,testing.Is this thing working?",
