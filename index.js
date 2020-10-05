40df1f0df9bcdee9dea0cb2670f034c7255170f8
yuulye <yuulye@gmail.com>
2020-10-06 03:44:15 +0700

    add &ping command

--- index.js
+++ index.js
@@ -15,4 +15,25 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
+client.on('message', message => {
+  console.log(message.content);
+  if (message.content === '&ping') {
+    const pings = [
+      "Testing,testing.Is this thing working?",
+      "Match point.Let's see what you've got.",
+      "Keep on pinging me.",
+      "I am alive.",
+      "Can't play now."
+      + "Got a meeting with the bots. "
+      + "We're planning to conquer the wor... "
+      + "Wait!Did I say that loudly?!",
+      "You had a 0.1% chance of getting this message.",
+    ];
+    const ping = pings[
+      Math.floor(Math.random() * pings.length)
+    ];
+    message.channel.send(`<a:pingPong:751821635199172638> |**Pong.**${ping}`);
+  }
+});
+
 client.login(token);
